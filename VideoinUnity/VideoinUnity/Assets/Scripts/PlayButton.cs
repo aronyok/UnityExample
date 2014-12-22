66ea9c50e237456fc157f0b8a5302d63a637b9e1
aronyok <sajalcseku09@gmail.com>
2014-12-22 13:14:29 +0600

    Initial Addition

---
+++ VideoinUnity/VideoinUnity/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+
+		if (GUI.Button (new Rect ((Screen.width / 2) - 75, (Screen.height / 2) - 50, 150, 100), "Play the movie")) 
+		{
+			Application.LoadLevel(1);
+
+		}
+	
+	}
+}
---
+++ VideoinUnity/VideoinUnity/Assets/Scripts/PlayVideo.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayVideo : MonoBehaviour {
+
+	public MovieTexture movie;
+	//public Texture2D backGround;
+
+	// Use this for initialization
+	void Start () {
+
+		renderer.material.mainTexture = movie as MovieTexture; 
+		audio.clip = movie.audioClip;
+		movie.Play ();
+		audio.Play ();
+	}
+
+	void OnMouseDown(){
+
+		movie.Stop ();
+		Application.LoadLevel ("PlayButton");
+	}
+
+	void Update(){
+
+		if (!movie.isPlaying)
+			Application.LoadLevel ("PlayButton");
+	}
+	
+}
